efcafd2808fbc046e58bc7fc2120b1a78b20420d
Aleksandr Andreev <andreev@reksoft.ru>
2023-02-13 00:03:07 +0300

    feat: добавлена тестовая среда

---
+++ src/shared/lib/class-names.test.ts
@@ -0,0 +1,28 @@
+import {describe, expect, test} from '@jest/globals'
+import {classNames} from './class-names'
+
+describe('classNames', () => {
+  test('with first param', () => {
+    expect(classNames('lol-kek')).toBe('lol-kek')
+  })
+
+  test('with array params', () => {
+    expect(classNames(['lol', 'kek'])).toBe('lol kek')
+  })
+
+  test('with additional params', () => {
+    expect(classNames('lol', {}, ['kek', 'cheburek'])).toBe('lol kek cheburek')
+  })
+
+  test('with mod params', () => {
+    expect(classNames('lol', {hovered: true, active: true}, ['kek', 'cheburek'])).toBe(
+      'lol kek cheburek hovered active',
+    )
+  })
+
+  test('with mod params', () => {
+    expect(classNames('lol', {hovered: false, active: true}, ['kek', 'cheburek'])).toBe(
+      'lol kek cheburek active',
+    )
+  })
+})
